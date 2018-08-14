b8f42b312cdaea1b3e429be0a3193600f500c195
Russell Wakefield <RJWakefield@gmail.com>
2018-08-14 12:57:47 -0400

    Create tabs table
    This closes #17 by creating a table for tabs. This includes adding
    database migration, model, and test.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180811200753) do
+ActiveRecord::Schema.define(version: 20180814163726) do
 
   create_table "chords", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20180811200753) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_chords_on_name", unique: true
+  end
+
+  create_table "tabs", force: :cascade do |t|
+    t.string   "name"
+    t.text     "encoded_song"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["name"], name: "index_tabs_on_name", unique: true
   end
 
 end
